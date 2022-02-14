dcf934ef6db264a59948ea96c108ff7cf8f7c559
cmdtrwd <99168914+cmdtrwd@users.noreply.github.com>
2022-02-14 10:53:53 +0700

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
+
+//------load page-----
+
+
+
+
+
+//------cursor---///
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
